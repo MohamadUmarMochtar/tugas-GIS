37bb04909b7139ba93f2db66f20cbcf42af293c3
MohamadUmarMochtar <mohamadumar40@gmail.com>
2018-07-24 01:20:13 +0700

    CP2
    CP2 aplikasi coffe shop surabaya

---
+++ CP2/app/src/main/java/com/coffee/shop/coffeeshop/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.coffee.shop.coffeeshop
+
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val c = Calendar.getInstance()
+        val year:Int =c.get(Calendar.YEAR)
+        val month:Int =c.get(Calendar.MONTH)
+        val day:Int =c.get(Calendar.DAY_OF_MONTH)
+        label.text = "$day/$month/$year"
+
+        fun readData(): MutableList<ArrayModel> {
+            val list = ArrayList<ArrayModel>()
+            val teks : String = atlokasi.text.toString()
+            list.add(ArrayModel(1, "Kopi Ganes", "-7.2875357", "112.8040991"))
+            list.add(ArrayModel(2, "Flodo Coffee", "-7.2570412", "112.7472578"))
+            list.add(ArrayModel(3, "Kedai Rhytm", "-7.291672", "112.805263"))
+            list.add(ArrayModel(4, "Rindu Kopi", "-7.2965329", "112.7640688"))
+            list.add(ArrayModel(5, "Kopi Kalamula", "-7.2695218", "112.7652111"))
+            list.add(ArrayModel(6, "Kedai Sipulung", "-7.300399", "112.765855"))
+            for (n : ArrayModel in list ){
+                if (teks == n.lokasi){
+                    tvlong.setText(n.long)
+                    tvlat.setText(n.lat)
+                    Toast.makeText(this, "Data Ditemukan", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return list
+        }
+
+
+        btntampil.setOnClickListener{readData()}
+        //memindah activity_main ke maps
+
+        btnganes.setOnClickListener {
+            val maintomap2 = Intent(this, MapsActivity::class.java)
+            startActivity(maintomap2)
+        }
+        btnrhytm.setOnClickListener {
+            val maintomap2 = Intent(this, MapsActivity2::class.java)
+            startActivity(maintomap2)
+        }
+        btnflodo.setOnClickListener {
+            val maintomap3 = Intent(this, MapsActivity3::class.java)
+            startActivity(maintomap3)
+        }
+        btnrindu.setOnClickListener {
+            val maintomap4 = Intent(this, MapsActivity4::class.java)
+            startActivity(maintomap4)
+        }
+        btnkalamula.setOnClickListener {
+            val maintomap5 = Intent(this, MapsActivity5::class.java)
+            startActivity(maintomap5)
+        }
+        btnsipulung.setOnClickListener {
+            val maintomap6 = Intent(this, MapsActivity6::class.java)
+            startActivity(maintomap6)
+        }
+    }
+
+}
---
+++ CP2/app/src/main/java/com/coffee/shop/coffeeshop/MapsActivity4.kt
@@ -0,0 +1,44 @@
+package com.coffee.shop.coffeeshop
+
+import android.graphics.BitmapFactory
+import android.location.Location
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MapsActivity4 : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps4)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val btnrindu = LatLng(-7.2965329, 112.7640688)
+        mMap.addMarker(MarkerOptions().position(btnrindu).title("Marker in Rindu Kopi"))
+        var zoomLevel = 16.0f
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(btnrindu, zoomLevel))
+    }
+}
